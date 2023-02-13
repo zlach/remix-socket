daa0d3cf5ae30c7bfa30fb30a655addb8551d88b
Zach Goldberg <zachgoldberg@Zachs-MacBook-Pro.local>
2023-02-12 20:48:43 -0600

    fixed login

--- app/routes/account.tsx
+++ app/routes/account.tsx
@@ -4,31 +4,19 @@ import { Outlet, useNavigate } from "@remix-run/react";
 import { useEffect } from "react";
 
 export default function Account() {
+  const navigate = useNavigate();
+
   useEffect(() => {
     async function retrieveAuthUser() {
       try {
         await Auth.currentAuthenticatedUser()
-        console.log('success?')
+        navigate('/chat')
       } catch (error) {
-        // do nothing
+        // do nothing...
       }
     }
     retrieveAuthUser()
-  }, [])
-
-  // const navigate = useNavigate();
-  // Hub.listen('auth', ({ payload }) => {
-  //   const { event } = payload
-  //   if (event === 'autoSignIn') {
-  //     console.log('autosignin worked')
-  //     navigate('/chat')
-  //   } else if (event === 'autoSignIn_failure') {
-  //     console.log('autosignin failed')
-  //     // redirect to sign in page\
-  //     navigate('/')
-  //   }
-  // })
-
+  }, [navigate])
 
   return (
     <main>
--- app/routes/account/index.tsx
+++ app/routes/account/index.tsx
@@ -1,19 +1,8 @@
 import { Auth } from "aws-amplify";
-import { useEffect, useState } from "react";
-import {
-  ActionArgs, redirect,
-} from "@remix-run/node";
+import type { SyntheticEvent} from "react";
+import { useState } from "react";
 import { Hub } from 'aws-amplify'
 import { useNavigate } from "@remix-run/react";
-import { badRequest, goodRequest } from "~/utils/request.server";
-
-import { prisma } from "~/db.server";
-
-import {
-  Link,
-  useActionData,
-  Form,
-} from "@remix-run/react";
 
 Hub.listen('auth', ({ payload }) => {
   const { event } = payload
@@ -27,135 +16,62 @@ Hub.listen('auth', ({ payload }) => {
   }
 })
 
-function validateEmail(email: unknown) {
-  if (typeof email !== "string" || !email.includes("@")) {
-    return `Must be valid email`;
-  }
-}
-
-function validatePassword(password: unknown) {
-  if (typeof password !== "string") {
-    return `Must be valid password`;
-  }
-}
-
-export const action = async ({ request }: ActionArgs) => {
-  const form = await request.formData();
-  const loginType = form.get("loginType");
-  const email = form.get("email");
-  const password = form.get("password");
-  const code = form.get("code");
-
-  if (
-    typeof loginType !== "string" ||
-    typeof email !== "string" ||
-    typeof password !== "string"
-  ) {
-    return badRequest({
-      fieldErrors: null,
-      fields: null,
-      successMessage: null,
-      formError: `Form not submitted correctly.`,
-    });
-  }
-
-  const fields = { loginType, email, password };
-  const fieldErrors = {
-    email: validateEmail(email),
-    password: validatePassword(password),
-  };
-  if (Object.values(fieldErrors).some(Boolean)) {
-    return badRequest({
-      fieldErrors,
-      fields,
-      successMessage: null,
-      formError: null,
-    });
-  }
-
-  switch (loginType) {
-    case "login": {
-      const user = await Auth.signIn(email, password)
-      if (!user) {
-        return badRequest({
-          fieldErrors: null,
-          fields,
-          successMessage: null,
-          formError: `Email/Password combination is incorrect`,
-        });
-      }
-      return redirect('/chat');
-    }
-    case "register": {
-      if (code && typeof code !== "string") {
-        return badRequest({
-          fieldErrors: null,
-          fields: null,
-          successMessage: null,
-          formError: `Form not submitted correctly.`,
-        });
-      } else if (code) {
-        await Auth.confirmSignUp(email, code)
-
-        return goodRequest({
-          successMessage: `Confirm Success`,
-          fields,
-          fieldErrors: null,
-          formError: null,
-        });
-      }
-
-      const userExists = await prisma.user.findFirst({
-        where: { email },
-      });
-
-      if (userExists) {
-        return badRequest({
-          fieldErrors: null,
-          fields,
-          successMessage: null,
-          formError: `User with email ${email} already exists`,
-        });
-      }
-      
-      await Auth.signUp({
-        username: email,
-        password,
-        attributes: {
-          email,
-        },
-        autoSignIn: {
-          enabled: true,
-        },
-      })
-
-      return goodRequest({
-        successMessage: `Register Success`,
-        fields,
-        fieldErrors: null,
-        formError: null,
-      });
-    }
-    default: {
-      return badRequest({
-        fieldErrors: null,
-        fields,
-        successMessage: null,
-        formError: `Login type invalid`,
-      });
-    }
-  }
-};
-
 export default function Login() {
-  const actionData = useActionData<typeof action>();
+  const [loginType, setLoginType] = useState<string>('login')
+  const [password, setPassword] = useState<string>('')
+  const [email, setEmail] = useState<string>('')
+  const [code, setCode] = useState<string>('')
+
+
   const [showCode, setShowCode] = useState<boolean>();
+  // const navigate = useNavigate();
+
+  const handleSubmit = async (event: SyntheticEvent) => {
+    event.preventDefault()
+
+    console.log(loginType)
+  
+    switch (loginType) {
+      case "login": {
+        const user = await Auth.signIn(email, password)
+        if (!user) {
+          return;
+        }
+        return navigate('/chat');
+      }
+      case "register": {
+        if (code && typeof code !== "string") {
+          return;
+        } else if (code) {
+          await Auth.confirmSignUp(email, code)
+  
+          return navigate('/chat')
+        }
+        
+        await Auth.signUp({
+          username: email,
+          password,
+          attributes: {
+            email,
+          },
+          autoSignIn: {
+            enabled: true,
+          },
+        })
+  
+        return setShowCode(true);
+      }
+      default: {
+        return;
+      }
+    }
+  };
 
   return (
     <div className="container">
       <div className="content" data-light="">
         <h1>Login</h1>
-        <Form method="post">
+        <form onSubmit={handleSubmit}>
           <fieldset>
             <legend className="sr-only">
               Login or Register?
@@ -166,9 +82,10 @@ export default function Login() {
                 name="loginType"
                 value="login"
                 defaultChecked={
-                  !actionData?.fields?.loginType ||
-                  actionData?.fields?.loginType === "login"
+                  loginType ===
+                  "login"
                 }
+                onChange={() => setLoginType(type => type === "login" ? "register" : "login")}
               />{" "}
               Login
             </label>
@@ -178,9 +95,10 @@ export default function Login() {
                 name="loginType"
                 value="register"
                 defaultChecked={
-                  actionData?.fields?.loginType ===
+                  loginType ===
                   "register"
                 }
+                onChange={() => setLoginType(type => type === "login" ? "register" : "login")}
               />{" "}
               Register
             </label>
@@ -191,25 +109,9 @@ export default function Login() {
               type="text"
               id="email-input"
               name="email"
-              defaultValue={actionData?.fields?.email}
-              aria-invalid={Boolean(
-                actionData?.fieldErrors?.email
-              )}
-              aria-errormessage={
-                actionData?.fieldErrors?.email
-                  ? "email-error"
-                  : undefined
-              }
+              value={email}
+              onChange={e => setEmail(e.target.value)}
             />
-            {actionData?.fieldErrors?.email ? (
-              <p
-                className="form-validation-error"
-                role="alert"
-                id="email-error"
-              >
-                {actionData.fieldErrors.email}
-              </p>
-            ) : null}
           </div>
           <div>
             <label htmlFor="password-input">Password</label>
@@ -217,48 +119,24 @@ export default function Login() {
               id="password-input"
               name="password"
               type="password"
-              defaultValue={actionData?.fields?.password}
-              aria-invalid={Boolean(
-                actionData?.fieldErrors?.password
-              )}
-              aria-errormessage={
-                actionData?.fieldErrors?.password
-                  ? "password-error"
-                  : undefined
-              }
+              value={password}
+              onChange={e => setPassword(e.target.value)}
             />
-            {actionData?.fieldErrors?.password ? (
-              <p
-                className="form-validation-error"
-                role="alert"
-                id="password-error"
-              >
-                {actionData.fieldErrors.password}
-              </p>
-            ) : null}
           </div>
-          {showCode || actionData?.successMessage === "Register Success" ? <div>
+          {showCode ? <div>
             <label htmlFor="code-input">Code</label>
             <input
               type="text"
               id="code-input"
               name="code"
+              value={code}
+              onChange={e => setCode(e.target.value)}
             />
           </div> : <div style={{ cursor: 'pointer', color: 'blue' }} onClick={() => setShowCode(true)}>Show Confirmation Code Input</div>}
-          <div id="form-error-message">
-            {actionData?.formError ? (
-              <p
-                className="form-validation-error"
-                role="alert"
-              >
-                {actionData.formError}
-              </p>
-            ) : null}
-          </div>
           <button type="submit" className="button">
             Submit
           </button>
-        </Form>
+        </form>
       </div>
       <div>
         <br/>
